aa67e0040d2212bcf8063910af85bf66ccd34651
marcoburgos <marko_burgos@gmail.com>
2020-10-18 21:26:09 -0400

    predeploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ayeProject.wsgi --log-file -
--- ayeProject/settings.py
+++ ayeProject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'zp)$in&^x=-29t@lq*mxt1d48(**z_5v8nl+q=4%4x-@$ik3qp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boda-andreayedgar-stgg.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,5 +133,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
